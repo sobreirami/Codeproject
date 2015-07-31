88cd10fd32b1fa19b143e12f9c9612c7718b54b5
Michel Sobreira <sobreira.michel@gmail.com>
2015-07-31 20:06:29 -0300

    exibindo informações das chaves estrangerias no metodo show

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -57,7 +57,7 @@ class ProjectController extends Controller
      */
     public function show($id)
     {
-        return $this->repository->find($id);
+        return $this->repository->with(['owner', 'client'])->find($id);
     }
 
     /**
