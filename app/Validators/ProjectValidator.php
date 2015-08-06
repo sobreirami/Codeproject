cc866c4ff755987a34ee3f69db4d877a5de32497
Michel Sobreira <sobreira.michel@gmail.com>
2015-08-06 17:34:16 -0300

    Adicionando validadores em project

--- app/Validators/ProjectValidator.php
+++ app/Validators/ProjectValidator.php
@@ -10,8 +10,8 @@ class ProjectValidator extends LaravelValidator
         'owner_id' => 'required|integer',
         'client_id' => 'required|integer',
         'name' => 'required|max:255',
-        'progress' => 'required',
-        'status' => 'required',
-        'due_date' => 'required'
+        'progress' => 'required|integer',
+        'status' => 'required|integer',
+        'due_date' => 'required|date'
     ];
 }
